f66d5279c2b936106705dc47f2bf399894d5f959
cipepser <respepic@gmail.com>
2019-01-05 13:46:56 +0900

    improve error handling

--- url/src/main.rs
+++ url/src/main.rs
@@ -1,16 +1,21 @@
 extern crate url;
+extern crate failure;
 
 use url::percent_encoding::percent_decode;
+use failure::Error;
 
-fn main() {
+pub type Result<T> = std::result::Result<T, Error>;
+
+fn main() -> Result<()> {
     let args: Vec<String> = std::env::args().collect();
     let input = &args[1];
 
-    println!("{}", decode(input));
+    Ok(println!("{}", decode(input)?))
 }
 
-fn decode(input: &str) -> String {
-    percent_decode(input.as_bytes()).decode_utf8().unwrap().to_string()
+fn decode(input: &str) -> Result<String> {
+    let decoded = percent_decode(input.as_bytes()).decode_utf8()?;
+    Ok(decoded.to_string())
 }
 
 #[cfg(test)]
@@ -21,7 +26,7 @@ mod tests {
     fn decode_space_ok() {
         let expected = "foo bar";
         let input = "foo%20bar";
-        let actual = decode(input);
+        let actual = decode(input).unwrap();
         assert_eq!(expected, actual);
     }
 
@@ -29,7 +34,7 @@ mod tests {
     fn decode_ascii_ok() {
         let expected = r##" !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ `"##;
         let input = r##"%20%21%22%23%24%25%26%27%28%29%2A%2B%2C%2D%2E%2F0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ `"##;
-        let actual = decode(input);
+        let actual = decode(input).unwrap();
         assert_eq!(expected, actual);
     }
 
@@ -37,6 +42,6 @@ mod tests {
     #[should_panic]
     fn decode_invalid_utf8_ng() {
         let input = "%93%FA%96%7B%8C%EA%0D%0A";
-        decode(input);
+        decode(input).unwrap();
     }
 }
